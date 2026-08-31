09ea0da7ff0dcdff6b9d9d6c24027dc57a526428
qwww-12 <laro1laro34@gmail.com>
2026-08-31 19:40:37 +0100

    first info about player

--- libs.h
+++ libs.h
@@ -5,5 +5,21 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <string.h>
+
+
+# define REQ_NAME "Enter your name: "
+# define REQ_GEN "Enter your gender F or M: "
+# define BAD_NAME "Your length name greater than ten, try again: "
+# define BAD_GEN "Enter M or F: "
+# define MAX_NAME_SIZE 10
+# define BUFFER_SIZE 256
+char _buffer[BUFFER_SIZE];
+
+typedef struct Player {
+	char *name;
+	char gender;
+//	_role *role;
+} Player;
 
 #endif
--- main.c
+++ main.c
@@ -1,9 +1,30 @@
 #include "libs.h"
 
+void first_info_handling(Player *player) {
+	write(STDOUT_FILENO, REQ_NAME, strlen(REQ_NAME));
+	while (true) {
+		read(STDIN_FILENO, _buffer, BUFFER_SIZE);
+		if (strlen(_buffer) - 1 > MAX_NAME_SIZE) {
+			write(STDERR_FILENO, BAD_NAME, strlen(BAD_NAME));
+			continue;
+		}
+		player->name = _buffer;
+		break;
+	}
+	write(STDOUT_FILENO, REQ_GEN, strlen(REQ_GEN));
+	while (true) {
+		read(STDOUT_FILENO, _buffer, BUFFER_SIZE);
+		if (strlen(_buffer) - 1 != 1 || (_buffer[0] != 'M' && _buffer[1] != 'F')) {
+			write(STDERR_FILENO, BAD_GEN, strlen(BAD_GEN));
+			continue;
+		}
+		player->gender = _buffer[0];
+		break;
+	}
+}
+
 int main(){
-	SCREEN *scr = newterm(NULL, stdout, stdin);
-	printw("nah");
-	refresh();
-	getch();
-	endwin();
+	Player *player = (Player *)malloc(sizeof(Player));
+	first_info_handling(player);
+	free(player);
 }
