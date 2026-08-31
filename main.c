9e01d6155f4582e857c12416cca98c3afca94463
qwww-12 <laro1laro34@gmail.com>
2026-08-31 21:03:16 +0100

    fix issue of clearing buffer when user enter a wrong input

--- main.c
+++ main.c
@@ -5,22 +5,28 @@ void first_info_handling(Player *player) {
 	while (true) {
 		read(STDIN_FILENO, _buffer, BUFFER_SIZE);
 		if (strlen(_buffer) - 1 > MAX_NAME_SIZE) {
+			memset(_buffer, 0, strlen(_buffer));
 			write(STDERR_FILENO, BAD_NAME, strlen(BAD_NAME));
 			continue;
 		}
 		player->name = _buffer;
 		break;
 	}
+	memset(_buffer, 0, strlen(_buffer));
 	write(STDOUT_FILENO, REQ_GEN, strlen(REQ_GEN));
 	while (true) {
 		read(STDOUT_FILENO, _buffer, BUFFER_SIZE);
-		if (strlen(_buffer) - 1 != 1 || (_buffer[0] != 'M' && _buffer[1] != 'F')) {
+		if (strlen(_buffer) - 1 != 1 || (_buffer[0] != 'M' && _buffer[0] != 'F')) {
+			memset(_buffer, 0, strlen(_buffer));
 			write(STDERR_FILENO, BAD_GEN, strlen(BAD_GEN));
 			continue;
 		}
 		player->gender = _buffer[0];
 		break;
 	}
+	memset(_buffer, 0, strlen(_buffer));
+	printf("name: %s", player->name);
+	printf("gender: %c", player->gender);
 }
 
 int main(){
