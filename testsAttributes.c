ad11bc6a13693075aaecb1689d1ade391e971931
qwww-12 <laro1laro34@gmail.com>
2026-09-07 02:50:54 +0100

    tested attributes

---
+++ testsAttributes.c
@@ -0,0 +1,25 @@
+#include <ncurses.h>
+
+int main(){
+	initscr();
+	addch('A' | A_STANDOUT);
+	addch('\n');
+	addch('A' | A_BLINK);
+	addch('\n');
+	addch('A' | A_DIM);
+	addch('\n');
+	addch('A' | A_BOLD);
+	addch('\n');
+	addch('A' | A_PROTECT);
+	addch('\n');
+	addch('A' | A_INVIS);
+	addch('\n');
+	addch('A' | A_ALTCHARSET);
+	addch('\n');
+	addch('A' | A_CHARTEXT);
+	addch('\n');
+	addch('A' | COLOR_PAIR(117));
+	refresh();
+	getch();
+	endwin();
+}
