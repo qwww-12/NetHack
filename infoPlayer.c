a1d5a2ce26104e8185712ad7476e505126113e8c
qwww-12 <laro1laro34@gmail.com>
2026-09-08 16:44:00 +0100

    restructure the way how print status of player on window by little lines and performance

--- infoPlayer.c
+++ infoPlayer.c
@@ -23,37 +23,37 @@ void takeNewName(char *name) {
 void PrintDataStructure(Player *player, WINDOW *win, char c) {
 	mvwprintw(win, 1, 1, "     name: %s", player->name);
 
-	if (!player->pet) {
-		if (c == 'p')
-			mvwprintw(win, 3, 1, "     apet: choosing now");
-		else
+	if (c == 'p')
+		mvwprintw(win, 3, 1, "     apet: choosing now");
+	else {
+		if (!player->pet)
 			mvwprintw(win, 3, 1, "     apet: not specified");
-	}
-	else
-		mvwprintw(win, 3, 1, "     apet: %s", player->pet);
-	if (!player->pet) {
-		if (c == 'r')
-			mvwprintw(win, 2, 1, "     role: choosing now");
 		else
-			mvwprintw(win, 2, 1, "     role: not specified");
+			mvwprintw(win, 3, 1, "     apet: %s", player->pet);
 	}
-	else
-		mvwprintw(win, 2, 1, "     role: ");
-	if (!player->gender) {
-		if (c == 'g')
-			mvwprintw(win, 4, 1, "   gender: choosing now");
-		else
+	//if (!player->pet) {
+	//	if (c == 'r')
+	//		mvwprintw(win, 2, 1, "     role: choosing now");
+	//	else
+	//		mvwprintw(win, 2, 1, "     role: not specified");
+	//}
+	//else
+	//	mvwprintw(win, 2, 1, "     role: ");
+	if (c == 'g')
+		mvwprintw(win, 4, 1, "   gender: choosing now");
+	else {
+		if (!player->gender)
 			mvwprintw(win, 4, 1, "   gender: not specified");
-	}
-	else
-		mvwprintw(win, 4, 1, "   gender: %s", player->gender);	
-	if (!player->naturale) {
-		if (c == 'n')
-			mvwprintw(win, 5, 1, " naturale: choosing now");
 		else
-			mvwprintw(win, 5, 1, " naturale: not specified");
+			mvwprintw(win, 4, 1, "   gender: %s", player->gender);
 	}
-	else
-		mvwprintw(win, 5, 1, " naturale: %s", player->naturale);
+	if (c == 'n')
+		mvwprintw(win, 5, 1, " naturale: choosing now");
+	else {
+		if (!player->naturale)
+			mvwprintw(win, 5, 1, " naturale: not specified");
+		else
+			mvwprintw(win, 5, 1, " naturale: %s", player->naturale);
+	}
 	wrefresh(win);
 }
