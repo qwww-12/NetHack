f180b2b31aee6c97719716b9338d471b1feed430
qwww-12 <laro1laro34@gmail.com>
2026-09-08 16:46:41 +0100

    delete the variable for use little of rram and performance

--- controllerOptions.c
+++ controllerOptions.c
@@ -1,20 +1,15 @@
 #include "libs.h"
 
 void manipulateOptWindow(Player *player, char inputC) {
-	char ret = inputC;
-
 	while (1) {
-		if (ret != 0) {
-			if (ret == 'q')
-				break;
-			WindowInfo(player, ret);
-			ret = 0;
+		if (inputC != 0) {
+			if (inputC == 'q')
+				return ;
+			WindowInfo(player, inputC);
 		}
 		if (inputC == 'g')
-			ret = genderWindow(player->gender);
-		if (inputC == 'p') {
-			ret = petWindow(&player->pet);
-			mvprintw(LINES - 1, 1, "ret = %c", ret);
-		}
+			inputC = genderWindow(&player->gender);
+		else if (inputC == 'p')
+			inputC = petWindow(&player->pet);
 	}
 }
